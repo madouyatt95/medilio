5e0d0e25c550c083eca324cfd4ec34204a8d9858
madouyatt95 <madouyatt95@gmail.com>
2026-05-05 10:54:18 +0200

    fix: update home screen icons and apple-touch-icon

--- vite.config.js
+++ vite.config.js
@@ -19,11 +19,12 @@ export default defineConfig({
             src: '/logo-medilio.png',
             sizes: '192x192',
             type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: '/logo-medilio.png',
             sizes: '512x512',
-            type: 'image/png',
+            type: 'image/png'
           }
         ],
       },
